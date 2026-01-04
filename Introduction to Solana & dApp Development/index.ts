f27480feb867fc2d423fffc36508b29369d6007e
arjunkumar811 <kumararjun26401@gmail.com>
2026-01-04 23:04:45 +0530

    Keypair generate

---
+++ Introduction to Solana & dApp Development/index.ts
@@ -0,0 +1,10 @@
+import { Keypair } from "@solana/web3.js";
+
+async function main() {
+const keypair = Keypair.generate();
+console.log(keypair.publicKey.toBase58());
+console.log(keypair.secretKey);
+
+}
+
+main();
